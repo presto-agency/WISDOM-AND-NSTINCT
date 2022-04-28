e05628f7f36fd6d1d53b877dcc29383632a5bb17
Kamarhinanton <Kamarhin_anton@ukr.net>
2022-04-28 13:01:05 +0300

    feat: add modification text

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,14 +1,16 @@
  const runAnimation = () => {
-     let animationObj = document.querySelector('.dashboard');
-     let addActive = () => {
-         animationObj.classList.add('active');
-         setTimeout(removeActive, 9200);
-     }
-     let removeActive = () => {
-         animationObj.classList.remove('active');
-     }
-     addActive();
-     setInterval(addActive, 9300);
+     let animationObjs = document.querySelectorAll('.dashboard');
+     animationObjs.forEach(animationObj => {
+         let addActive = () => {
+             animationObj.classList.add('active');
+             setTimeout(removeActive, 9200);
+         }
+         let removeActive = () => {
+             animationObj.classList.remove('active');
+         }
+         addActive();
+         setInterval(addActive, 9300);
+     })
  }
 runAnimation();
 
